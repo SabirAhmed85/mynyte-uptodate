081a2bb3daba54980ca8d6ed6b246adfe3ecc9d2
Sabir Ahmed <sabir.ahmed.web@gmail.com>
2017-03-21 23:14:43 +0000

    main app master initial setup

--- platforms/android/assets/www/data/db-result-close.php
+++
@@ -1,6 +0,0 @@
-<?php
-  while ($row = mysql_fetch_assoc($result))
-    $output[] = $row;
-  print(json_encode($output));
-  mysql_close();
-?>
--- platforms/ios/www/data/sp/unreferenced/getPersonDetail.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $id = $_GET['id'];
-
-  $sql    = "SELECT * FROM people WHERE id == {{$id}}" ;
-
-  $result = mysql_query($sql);
-?>
