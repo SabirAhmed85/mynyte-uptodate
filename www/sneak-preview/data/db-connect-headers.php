d77d47ac3f27198b1f2ca3d5b1ed4f04dfaaa76b
Sabir Ahmed <sabir.ahmed.web@gmail.com>
2017-04-02 15:47:40 +0100

    new file structure for db connection

--- www/sneak-preview/data/db-connect-headers.php
+++ www/sneak-preview/data/db-connect-headers.php
@@ -1,24 +1,5 @@
 <?php
-  $intended_environment = 'Staging';
-
-  $http_origin = $_SERVER['HTTP_ORIGIN'];
-  $httpUrl = ($intended_environment == 'Staging') ? "https://www.mynyte.co.uk/staging": "https://www.mynyte.co.uk";
-
-  if ($http_origin == $httpUrl) {
-    header("Access-Control-Allow-Origin: $http_origin");
-  }
-
-  $db_host = "localhost";
-
-  $db_uid = "qxiryynz_mynyteuser";
-
-  $db_pass = "wM)Ln8-Q2o6g";
-
-  $db_name = ($intended_environment == 'Staging') ? "qxiryynz_mynyte": "qxiryynz_mynyte";
-  //$db_name = "MyNyte_MyTownCombinedTest";
-
-  //error_reporting(E_ALL);
-  //ini_set('display_errors',1);
+  require_once('main-connect.php');
   
   $db_con = mysql_connect($db_host, $db_uid, $db_pass) or die(mysql_error());
   
--- www/sneak-preview/data/db-connect.php
+++ www/sneak-preview/data/db-connect.php
@@ -1,26 +1,9 @@
 <?php
-
-  $intended_environment = 'Staging';
-
-  $http_origin = $_SERVER['HTTP_ORIGIN'];
-  $httpUrl = ($intended_environment == 'Staging') ? "https://www.mynyte.co.uk/staging": "https://www.mynyte.co.uk";
-
-  if ($http_origin == $httpUrl) {
-    header("Access-Control-Allow-Origin: $http_origin");
-  }
+  require_once('main-connect.php');
+  
   header('content-type: application/json; charset=utf-8');
   header('Access-Control-Allow-Methods: GET, SET, POST');
   header("Access-Control-Allow-Headers: X-Requested-With");
-
-  $db_host = "localhost";
-
-  $db_uid = "qxiryynz_mynyteuser";
-
-  $db_pass = "wM)Ln8-Q2o6g";
-
-  $db_name = ($intended_environment == 'Staging') ? "qxiryynz_mynyte": "qxiryynz_mynyte";
-  //$db_name = "MyNyte_MyTownCombinedTest";
-
 
   $db_con = mysql_connect($db_host, $db_uid, $db_pass) or die(mysql_error());
   mysql_select_db($db_name);
