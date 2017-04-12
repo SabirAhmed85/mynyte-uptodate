df5832cb1c8c7ac1040f6795e9219a3559fc5899
Sabir Ahmed <sabir.ahmed.web@gmail.com>
2017-04-12 11:44:59 +0100

    db-connect

---
+++ www/sneak-preview/db-connect.php
@@ -0,0 +1,28 @@
+<?php
+    $http_origin = $_SERVER['HTTP_ORIGIN'];
+
+    if ($http_origin == "http://www.mynyte.co.uk" || $http_origin == "http://localhost:8000")
+    {
+        header("Access-Control-Allow-Origin: $http_origin");
+    }
+    header('content-type: application/json; charset=utf-8');
+    header('Access-Control-Allow-Methods: GET, SET, POST');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+
+  $db_host = "localhost:3306";
+
+  $db_uid = "sabir_admin";
+
+  $db_pass = "The_edgesxa454";
+
+  $db_name = "MyNyte";
+  //$db_name = "MyNyte_MyTownCombinedTest";
+
+
+  $db_con = mysql_connect($db_host, $db_uid, $db_pass) or die(mysql_error());
+  
+  mysql_select_db($db_name);
+  
+  //Debug
+  //ini_set('display_errors', 1);
+?>
