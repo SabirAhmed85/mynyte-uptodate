48a3916e893fa29f1e780140ec8452b3a22ecdf3
SabirAhmed85 <sabir@mynyte.co.uk>
2017-09-12 14:40:15 +0100

    reomved business folder and updated css

--- business/templates/contact-template.php
+++
@@ -1,5 +0,0 @@
-<div class="text">
-    <h2 class="lower">Start Your Journey</h2>
-    <p>It's time to help your business make more money.</p>
-    <a href="https://www.mynyte.co.uk/business-staging/contact-us.php"><button>Start Your Journey</button></a>
-</div>
