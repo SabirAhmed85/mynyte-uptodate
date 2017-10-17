b6a4f6f42562ce27752ca848417b5c997d8aac4a
Sabir Ahmed <sabir.ahmed.web@gmail.com>
2017-10-17 22:41:26 +0100

    table booking to email functionality

--- www/sneak-preview/data/sp/TableBooking.php
+++ www/sneak-preview/data/sp/TableBooking.php
@@ -14,6 +14,22 @@
     $comment = (empty($_GET['comment'])) ? "": mysql_real_escape_string($_GET['comment']);
 	//echo json_encode("CALL createTableBooking($_usersProfileId, $_businessId, '$usersName', '$usersEmail', $tableFor, '$dateTimeRequested', '$comment');");
     $result = mysql_query("CALL createTableBooking($_usersProfileId, $_businessId, '$usersName', '$usersEmail', $tableFor, '$dateTimeRequested', '$comment');");
+
+    $output = mysql_fetch_assoc($result);
+    $email = $output['email'];
+
+    $rootUrl = ($intended_environment == 'Staging') ? 'https://www.mynyte.co.uk/staging/': 'https://www.mynyte.co.uk/';
+    $emailUrl = $rootUrl . 'sneak-preview/data/functions/email.php';
+    $myvars = 'action=informRestaurantOfTableBooking&email=' . $email;
+
+    $ch = curl_init( $emailUrl );
+    curl_setopt( $ch, CURLOPT_POST, 1);
+    curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
+    curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt( $ch, CURLOPT_HEADER, 0);
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $response = curl_exec( $ch );
   }
   else if ($action == 'getRequestedTableBookings') {
     $_businessId = (empty($_GET['_businessId'])) ? "": mysql_real_escape_string($_GET['_businessId']);
@@ -92,11 +108,16 @@
   }
   //print(json_encode($_usersId));
   if ($action != 'deleteBlockedTableBookingInterval'
-    && $action != 'createBlockedTableBookingInterval') {
+    && $action != 'createBlockedTableBookingInterval'
+    && $action != 'createTableBooking') {
     $output = null;
     while($row = mysql_fetch_assoc($result))
       $output[] = $row;
         
+    header('Content-Type: application/json');
+    echo json_encode($output);
+  }
+  else if ($action == 'createTableBooking') {
     header('Content-Type: application/json');
     echo json_encode($output);
   }
