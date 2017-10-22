fd2721c4b0109463a1357a6cde513d92d594789d
Sabir Ahmed <sabir.ahmed.web@gmail.com>
2017-10-22 15:33:34 +0100

    fonticon files for ion added

--- www/js/api/externalApi.js
+++ www/js/api/externalApi.js
@@ -618,7 +618,7 @@
 			$(document).ready(function() {
 				//General css files
 				$('head').append('<link rel="stylesheet" href="https://www.mynyte.co.uk/staging/css/api-style.css" type="text/css" />');
-				$('head').append('<link rel="stylesheet" href="https://www.mynyte.co.uk/staging/lib/ionic/css/ionic.min.css" type="text/css" />');
+				$('head').append('<link rel="stylesheet" href="https://www.mynyte.co.uk/staging/css/ionicons.min.css" type="text/css" />');
 				$('head').append('<link rel="stylesheet" href="https://webfonts.creativecloud.com/c/69721a/1w;quicksand,2,WXp:W:n4,WXn:W:n7/l" type="text/css" />');
 				$('head').append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css" />');
 				
