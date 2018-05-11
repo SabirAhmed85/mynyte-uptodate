ba575fe9827f73c100ca65b692dd28308aa227b5
SabirAhmed85 <sabir@mynyte.co.uk>
2018-05-11 16:54:00 +0100

    free section updated

---
+++ www/free/templates/footer-template.php
@@ -0,0 +1,20 @@
+<?php ?>
+<footer>
+    <img src="../../sneak-preview/img/n-logo.png" alt="MyNyte Logo" />
+    <p>MyNyte</p>
+    <ul>
+        <li class="col-12 col-sm-6 col-md-3">
+            <a target="_blank" href="https://www.mynyte.co.uk">HOME</a>
+        </li>
+        <li class="col-12 col-sm-6 col-md-3">
+            <a target="_blank" href="https://www.mynyte.co.uk/free/rules/">COMPETITION RULES</a>
+        </li>
+        <li class="col-12 col-sm-6 col-md-3">
+            <a target="_blank" href="https://www.mynyte.co.uk/#/app/privacyPolicy">PRIVACY POLICY</a>
+        </li>
+        <li class="col-12 col-sm-6 col-md-3">
+            <a href="#top">BACK TO TOP</a>
+        </li>
+    </ul>
+</footer>
+<?php ?>
---
+++ www/free/thank-you/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
+        
+        <title>MyNyte Competition Entry Thank You Page | MyNyte</title>
+        <meta name="description" content="MyNyte Competition Entry Complete - thanks for entering the MyNyte Competition giveaway!">
+        <meta name="keywords" content="MyNyte, competition, giveaway, contest, bedford, thank you, competition entry complete">
+        
+        <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+        <link href="../style.css" rel="stylesheet">
+        
+        <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<script>
+          (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-5236024249196570",
+            enable_page_level_ads: true
+          });
+        </script>
+    </head>
+	<body>
+    	<a name="top"></a>
+    	<article>
+        	<header>
+            	<div class="top-logo">
+                	<img src="https://surveymonkey-assets.s3.amazonaws.com/survey/151126625/dde8f9c2-ce42-4bee-b68f-beb82fbd3034.jpg" alt="MyNyte in Bedford" />
+                </div>
+                <div class="page-title">
+                	<h1>Thanks for your entry! Next steps for the MyNyte Giveaway...</h1>
+                </div>
+            </header>
+            <section>
+            	<h2>Thanks for Entering!</h2>
+                <p>You've now been entered into the MyNyte FREE Ipad giveaway. Make sure you stay up to date with <a href="https://www.facebook.com/MyNyte">our Facebook page</a> for the latest news about evening activity in Bedford, and updates about the competition.</p>
+                <ul>
+                	<li>
+                    	The competition will close on the 31st May 2018, and a winner will be announced on the 2nd June 2018.
+                    </li>
+                    <li>
+                    	The competition will close on the 31st May 2018, and a winner will be announced on the 2nd June 2018.
+                    </li>
+                </ul>
+            </section>
+            <section>
+            	<h2 class="section-break">Increase your chances of winning by spreading the word...</h2>
+                <p>We've also got some group/joint competitions in store for you, where the prizes are based on group bookings, free group activity days etc., so if you want to increase your chances of winning even more MyNyte competitions in the future, then make sure you spread the word and let your mates know.</p>
+                <ul class="tile-ul">
+                	<li class="col-6 col-sm-3">
+                    	<a>
+                        	<img src="../../sneak-preview/img/free-section/twitter-share.jpg" alt="Share on Twitter" />
+                    		<span>Share on Twitter</span>
+                        </a>
+                    </li>
+                    <li class="col-6 col-sm-3">
+                    	<a>
+                        	<img src="../../sneak-preview/img/free-section/whatsapp-share.png" alt="Share on WhatsApp" />
+                    		<span>Share in WhatsApp</span>
+                        </a>
+                    </li>
+                    <li class="col-6 col-sm-3">
+                    	<a>
+                        	<img src="../../sneak-preview/img/free-section/facebook-share.jpg" alt="Share on Facebook" />
+                    		<span>Share on Facebook</span>
+                        </a>
+                    </li>
+                    <li class="col-6 col-sm-3">
+                    	<a>
+                        	<img src="../../sneak-preview/img/free-section/email-share.jpg" alt="Share via E-mail" />
+                    		<span>Share via E-mail</span>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+            	<h2 class="section-break">Future Giveaways &amp; Competitions</h2>
+                <p>We'll be running a lot of different giveaways and competitions in the future, so make sure you keep an eye out if you want to be in with a chance of winning some great goodies and experiences. We're all about giving here at MyNyte! Future prizes will include things like:</p>
+                <ul>
+                	<li>
+                    	More app-based electronics
+                    </li>
+                    <li>
+                    	Fully-paid for events &amp; activity bookings for things going on in Bedford
+                    </li>
+                    <li>
+                    	Free Cineworld Unlimited Cards
+                    </li>
+                    <li>
+                    	Also free meals at some of the swankiest restaurants in Bedford
+                    </li>
+                </ul>
+                
+                <h3>Other ways to stay up to date with MyNyte Giveaways</h3>
+                <p>
+                	Even if you're not a regular to Facebook, there are lots of ways you can keep up to date with all our comps and giveaways, so you never have to miss out:
+                </p>
+                <ul>
+                	<li>
+                    	Look out for the MyNyte blog - <a target="_blank" href="https://www.mynyte.co.uk/blog">MyNyte Blog</a>
+                    </li>
+                    <li>
+                    	Sign up to the MyNyte Newsletter - <a target="_blank" href="https://www.mynyte.co.uk/blog">Sign up to the MyNyte Newsletter</a>
+                    </li>
+                    <li>
+                    	Follow MyNyte on Instagram - <a target="_blank" href="https://www.instagram.co.uk/themynyteapp">MyNyte on Instagram</a>
+                    </li>
+                    <li>
+                    	And of course, there's the Facebook page - <a target="_blank" href="https://www.facebook.com/MyNyte">MyNyte on Facebook</a>
+                    </li>
+                </ul>
+            </section>
+            <section class="last">
+            	<h2 class="section-break">Download MyNyte - Bedford's Evening-planning App...</h2>
+                <p>If you haven't already, make sure to download MyNyte, the all-in-one evening-planning app based in Bedford, from the Apple or the Google Play app stores. Plan your whole evening in Bedford, from booking restaurant tables &amp; event tickets, to finding out cinema showing times &amp; even booking taxis.</p>
+            </section>
+            <?php include_once("../templates/footer-template.php"); ?>
+            <script type="text/javascript" src="../../bootstrap/js/bootstrap.min.js"></script>
+        </article>
+    </body>
+</html>
