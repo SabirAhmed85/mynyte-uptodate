df4eb7d8a242e5e584dd7ee85c0ad33ff5b63fcf
SabirAhmed85 <sabir.ahmed@hotmail.co.uk>
2018-05-18 17:14:27 +0100

    free pages added

--- www/free/rules/index.php
+++ www/free/rules/index.php
@@ -18,6 +18,16 @@
             enable_page_level_ads: true
           });
         </script>
+        <style>
+            section p {
+                margin: 15px 0;
+            }
+
+            section ul {
+                margin: -15px 15px 10px;
+                width: calc(100% - 20px);
+            }
+        </style>
     </head>
 	<body>
     	<a name="top"></a>
@@ -32,14 +42,94 @@
             </header>
             <section>
             	<h2>Full Rules, Terms &amp; Conditions for the MyNyte Giveaway</h2>
+
+                <p>FROM MyNyte.co.uk</p>
+
+                <p>MyNyte.co.uk Giveaway</p>
+
+                <p>OFFICIAL RULES</p>
+
+                <p>NO PURCHASE IS NECESSARY TO ENTER OR WIN. MAKING A PURCHASE WILL NOT IMPROVE ENTRANT’S ODDS OF WINNING. SUBJECT TO ALL FEDERAL, STATE, LOCAL AND PROVINCIAL LAWS, REGULATIONS, AND ORDINANCES.</p>
+
+                <p>MyNyte.co.uk - this month - is giving away a new 2018 9.7" Apple iPad (32GB). Entrance for the giveaway begins at 12:01 AM Pacific Daylight Time (“PDT”) the first day of the announcement and continues until the winner is announced on the selected day which is updated monthly and listed here www.mynyte.co.uk/free/thank-you/ . (“Giveaway Period”).</p>
+
+                <p>1.  ELIGIBILITY: The MyNyte.co.uk Giveaway (the “Giveaway”) is open to legal residents of the United Kingdom only, who are the legal age of majority (18 years of age and over) as of the date and time of entry. Employees and directors of MyNyte.co.uk or any of their parent companies (or subsidiaries thereof), affiliates, representatives, consultants, contractors, legal counsel, advertising, public relations, promotional, fulfillment, and marketing agencies, and members of their immediate families (spouses, parents, children, siblings and their spouses, regardless of where they live) and those living in the same household are not eligible to enter. Participation constitutes Entrant’s full and unconditional agreement to these Official Rules and to Sponsor’s decisions, which are final and binding in all matters related to the Giveaway. Sponsor reserves the right to verify the eligibility of Winner. Any entry shall be rendered void if in contravention of these Giveaway Rules (“the Official Rules”) or where prohibited or restricted by law.</p>
+
+                <p>2.  TO ENTER: For a chance to win a Monthly Prize or the Grand Prize, participants (“Entrants”) may enter the Giveaway during the Giveaway Period by filling out completely and accurately an online entry form at www.mynyte.co.uk/free/ (the “Website”). Participation is LIMITED TO ONE (1) ENTRY PER PERSON PER GIVEAWAY PERIOD. In the event more than one (1) entry is received from any one (1) person during the giveaway period the first entry processed will be considered the only valid entry received. In the event of a dispute regarding an entry, entries will be deemed made by the authorized account holder of the e-mail address submitted at the time of entry. The authorized account holder is the natural person who is assigned to the e-mail address by an internet access provider or other organization that is responsible for assigning e-mail addresses or the domain associated with the submitted e-mail address.</p>
+
+                <p>3.  SELECTION AND NOTIFICATION OF WINNERS/ODDS: Monthly Drawing: Sponsor will select one (1) potential Winner using a random drawing method from the pool of all eligible entries received up until the end of the Giveaway Period and will exclude any entrant who has won a previous Monthly Prize Drawing. Information around the drawing date is located on this page: www.mynyte.co.uk/free/thank-you/ . Odds of being selected as a Winner are based on the total number of eligible entries received up until the date and time of the drawing.</p>
+
+                <p>The potential winner of each Prize Drawing will be notified by e-mail and Facebook Message within seven (7) days after the drawing in which their name was chosen as a potential winner and will be advised of the prize that they are eligible to win.</p>
+
+                <p>If any prize notification by Facebook Message or Email is not answered after 3 attempts, if the e-mail notification is returned as undeliverable, or if a potential winner is disqualified or otherwise determined to be ineligible or declines to accept the prize within five (5) days of notification, or refuses or fails to comply with the Acceptance Criteria detailed below, the prize will be forfeited and an alternate potential winner may be selected at random from the remaining eligible entries received.</p>
+
+                <p>4.  ACCEPTANCE CRITERIA: Potential winner must consent to each of the following Acceptance Criteria to be considered an official winner:
+
                 <ul>
-                	<li>
-                    	The competition will close on the 31st May 2018, and a winner will be announced on the 2nd June 2018.
-                    </li>
-                    <li>
-                    	The competition will close on the 31st May 2018, and a winner will be announced on the 2nd June 2018.
-                    </li>
+                    <li>a.  Provide a current residental address for themselves via e-mail;</li>
+
+                    <li>b.  Cover any delivery/transporting costs which go above and beyond the reasonable expected delivery charge for a resident of the U.K.</li>
                 </ul>
+                </p>
+
+                <p>5.  THE PRIZES:
+
+                <ul>
+                    <li>a.  PRIZE: One (1) Entrant will receive three (3) free nights at a condo in Destin, Florida. WINNER must use the accommodations within one (1) year of the date the prize was awarded. Vacation does not include meals, transportation, travel insurance, or any other expenses.</li>
+
+                    <li>b.  PRIZE VALUE: Prizes are currently valued at approximately $600.00 depending on the time of travel.</li>
+                </ul>
+                </p>
+
+                <p>6.  PRIZE RESTRICTIONS: Sponsor reserves the right to offer only the stated number of prizes by random drawing among all legitimate, un-awarded, eligible prize claims. No more than one (1) prize will be offered to an Entrant and only one (1) prize will be offered per household. Prize Winners will be removed from any further drawings and will not be eligible to receive any further prizes. The Prize Winners must have concluded all their communication and related administration within 12 months of receiving their award notification. Winner must be the individual receiving the prize on all occasions.</p>
+
+                <p>Prizes cannot be used in conjunction with any other promotion or offer. Prizes cannot be substituted, assigned, transferred, or redeemed for cash by Winner.</p>
+
+                <p>All members of the Winner’s party must be available to receive the delivery of the prize at a valid U.K address. Any and all fees and costs and all applicable regional and local taxes and expenses related to delivery, acceptance and use of the prizes not specifically stated herein, including but not limited to, air travel fees, other tariffs or duties, surcharges, excursions and/or other expenses which are above and beyond the reasonably expected amount, are solely the responsibility of the individual prize Winner.</p>
+
+                <p>All prize details are at Sponsor’s sole discretion. Sponsor is required by law to report the prize where required.</p>
+
+                <p>7.  GENERAL CONDITIONS: Sponsor, and its affiliated companies, may contact Entrants for marketing purposes by e-mail, landline telephone using an automatic dialer (for which express written consent is provided at the time of entry) or post regarding publications and information relating to its products, offers and services. Entrants consent to be contacting in such manner by entering to win. No purchase or any good or service is necessary or required to enter or win. Purchase or any good or service marketed to an Entrant will not increase the likelihood of winning. Entrants may withdraw consent at any time by sending a written email request to this effect to the address below:
+
+                <ul>
+                    <li>info@mynyte.co.uk</li>
+                    <li>Subject: OPT OUT</li>
+                </ul>
+                </p>
+
+                <p>All Entrants will conduct themselves in a decorous manner at all times during their participation in the Giveaway. Sponsor reserves the right to disqualify any Entrant if such Entrant has engaged in, or during the Giveaway engages in, any act which, in Sponsor’s sole judgment, involves moral turpitude or which brings Sponsor or Entrant into public disrepute, contempt, scandal or ridicule, or which damages, denigrates or injures the reputation or success of Sponsor or its products or services.</p>
+
+                <p>Sponsor does not reserve the right to cancel the Giveaway once announced. Sponsor may prohibit an Entrant from participating in the Giveaway or winning a prize if, in its sole discretion, it determines that Entrant is or has attempted to undermine the legitimate operation of the Giveaway by cheating, hacking, deception or other unfair playing practices or intending to annoy, abuse, threaten, or harass any other Entrants or Sponsor representatives.</p>
+
+                <p>WARNING: ANY ATTEMPT BY ANY PERSON TO DELIBERATELY DAMAGE ANY WEBSITE ASSOCIATED WITH THIS GIVEAWAY OR UNDERMINE THE LEGITIMATE OPERATION OF THE GIVEAWAY IS A VIOLATION OF CRIMINAL AND CIVIL LAWS AND SHOULD SUCH ATTEMPT BE MADE, SPONSOR RESERVES THE RIGHT TO SEEK DAMAGES FROM ANY SUCH PERSON TO THE FULLEST EXTENT PERMITTED BY LAW.</p>
+
+                <p>Sponsor’s failure to enforce any term of these Official Rules shall not constitute a waiver of that provision.</p>
+
+                <p>8.  RELEASE AND LIMITATIONS OF LIABILITY: By participating in this Giveaway, Entrants agree that Sponsor and their shareholders, affiliates, subsidiaries, representatives, consultants, contractors, legal counsel, advertising, public relations, promotional, fulfillment and marketing agencies, and their respective officers, directors, employees, representatives and agents (the “Released Parties”) are not responsible for:
+                    <ul>
+                        <li>a.  any incorrect or inaccurate information, whether caused by Entrants, printing errors or by any of the equipment or programming associated with or utilized in the Giveaway;</li>
+                        <li>b.  technical failures of any kind, including, but not limited to malfunctions, interruptions, or disconnections in phone lines or network hardware or software;</li>
+                        <li>c.  unauthorized human intervention in any part of the entry process or the Giveaway;</li>
+                        <li>d.  technical or human error which may occur in the administration of the Giveaway or the processing of entries; or</li>
+                        <li>e.  to the extent permitted by law, any injury, loss or damage of any kind to persons, including personal injury or death or property which may be caused, directly or indirectly, in whole or in part, from Entrant’s participation in the Giveaway, downloading any materials, acceptance, possession or receipt or use or misuse of any prize.</li>
+                    </ul>
+                </p>
+
+                <p>To the extent permitted by law, by participating in this Giveaway, Entrants agree that the Released Parties will have no liability whatsoever for, and shall be held harmless by Entrants against any claims based on publicity rights, defamation or invasion of privacy. Released Parties are not responsible if a prize cannot be awarded due to transporting/delivery/travel cancellations, delays, or interruptions due to acts of god, acts of war, natural disasters, weather, or terrorism.</p>
+
+                <p>9.  PUBLICITY GRANT: Except where prohibited by law, Winner grants to Sponsor (which grant will be confirmed in writing on request of Sponsor), its parent company and any subsidiary thereof, affiliates, retailers, distributors, advertising and promotion agencies, suppliers, and those acting pursuant to its authority, the right and permission to print, publish, broadcast, and use, worldwide in any media now known or hereafter developed, including but not limited to the world wide web, at any time or times, the Winner’s name, portrait, picture, city/town of residence, voice, likeness, opinions and biographical information for advertising, trade, and promotional purposes (including the announcement of his or her name on television or radio broadcast) without additional consideration, compensation, permission, or notification.</p>
+
+                <p>10. DISPUTES: To the extent permitted by law, Entrant agrees that: (1) any and all disputes, claims and causes of action arising out of or connected with this Giveaway or any prize awarded shall be resolved individually, without resort to any form of class action, and exclusively in the Courts of the United Kingdom; (2) any and all claims, judgments, and awards shall be limited to actual out-of-pocket costs incurred, including costs associated with entering the Giveaway, but in no event attorneys’ fees; (3) under no circumstances will Entrant be permitted to obtain awards for, and Entrant hereby waives all rights to claim, indirect, punitive, incidental and consequential damages and any other damages (other than for actual out-of-pocket expenses), any and all rights to have damages multiplied or otherwise increased.</p>
+
+                <p>All issues and questions concerning the construction, validity, interpretation and enforceability of these Official Rules, or the rights and obligations of the Entrant and Sponsor in connection with the Giveaway shall be governed by, and construed in accordance with, the laws of the United Kingdom.</p>
+
+                <p>11. PRIVACY: Information collected from Entrants is subject to the Sponsor’s Privacy Policy, which can be found at https://www.mynyte.co.uk/#/app/privacyPolicy.</p>
+
+                <p>12. MISCELLANEOUS: Any names and addresses acquired during the course of this Giveaway will be used for the purpose of soliciting additional services.</p>
+
+                <p>This Giveaway is neither an offer to sell nor a solicitation to residents in jurisdictions in which registration requirements have not been fulfilled, and your eligibility will depend upon your age.</p>
+
+                <p>THIS ADVERTISEMENT IS BEING USED FOR THE PURPOSE OF SOLICITING PRODUCTS AND SERVICES IN THE U.K.</p>
             </section>
             <?php include_once("../templates/footer-template.php"); ?>
         </article>
--- www/free/thank-you/index.php
+++ www/free/thank-you/index.php
@@ -27,6 +27,7 @@
         	<header>
             	<div class="top-logo">
                 	<img src="https://surveymonkey-assets.s3.amazonaws.com/survey/151126625/dde8f9c2-ce42-4bee-b68f-beb82fbd3034.jpg" alt="MyNyte in Bedford" />
+                    <h1>Entry Complete!</h1>
                 </div>
                 <div class="page-title">
                 	<h1>Thanks for your entry! Next steps for the MyNyte Giveaway...</h1>
@@ -34,13 +35,22 @@
             </header>
             <section>
             	<h2>Thanks for Entering!</h2>
-                <p>You've now been entered into the MyNyte FREE Ipad giveaway. Make sure you stay up to date with <a href="https://www.facebook.com/MyNyte">our Facebook page</a> for the latest news about evening activity in Bedford, and updates about the competition.</p>
+                <p>You've now been entered into the MyNyte FREE Ipad giveaway. Make sure you stay up to date with <a target="_blank" href="https://www.facebook.com/MyNyte">our Facebook page</a> for the latest news about evening activity in Bedford, and updates about the competition.</p>
                 <ul>
                 	<li>
-                    	The competition will close on the 31st May 2018, and a winner will be announced on the 2nd June 2018.
+                    	The competition will close on the 31st May 2018, and the competition winner for the month will be drawn on June 2nd, 2018. See Official Rules for more details.
                     </li>
                     <li>
-                    	The competition will close on the 31st May 2018, and a winner will be announced on the 2nd June 2018.
+                        The Winner will be announced on both our Facebook Fan Page and via our Newsletter on the day of the drawing!
+                    </li>
+                    <li>
+                        The prize is a completely FREE and brand new 2018 Apple iPad (32GB), worth over £300.
+                    </li>
+                    <li>
+                    	Look out for our emails which will contain more tips, giveaways and info on Bedford!
+                    </li>
+                    <li>
+                        Sharing on social media is appreciated!... we want to spread the word about our site and about MyNyte in Bedford!
                     </li>
                 </ul>
             </section>
@@ -48,26 +58,26 @@
             	<h2 class="section-break">Increase your chances of winning by spreading the word...</h2>
                 <p>We've also got some group/joint competitions in store for you, where the prizes are based on group bookings, free group activity days etc., so if you want to increase your chances of winning even more MyNyte competitions in the future, then make sure you spread the word and let your mates know.</p>
                 <ul class="tile-ul">
-                	<li class="col-6 col-sm-3">
-                    	<a>
+                	<li class="col-6 col-sm-3 col-md-4">
+                    	<a target="_blank" href="https://twitter.com/intent/tweet?text=Check out this great competition from MyNyte! It's at the top of their Facebook Page: www.facebook.com/MyNyte">
                         	<img src="../../sneak-preview/img/free-section/twitter-share.jpg" alt="Share on Twitter" />
                     		<span>Share on Twitter</span>
                         </a>
                     </li>
-                    <li class="col-6 col-sm-3">
-                    	<a>
+                    <li class="col-6 col-sm-3 d-block d-md-none">
+                    	<a href="whatsapp://send?text=Hey, check out this great competition from MyNyte! It's at the top of their Facebook Page: www.facebook.com/MyNyte" data-action="share/whatsapp/share">
                         	<img src="../../sneak-preview/img/free-section/whatsapp-share.png" alt="Share on WhatsApp" />
                     		<span>Share in WhatsApp</span>
                         </a>
                     </li>
-                    <li class="col-6 col-sm-3">
-                    	<a>
+                    <li class="col-6 col-sm-3 col-md-4">
+                    	<a data-message='Check out this competition!' href='http://www.facebook.com/sharer/share.php?u=https://www.facebook.com/MyNyte' target='_blank'>
                         	<img src="../../sneak-preview/img/free-section/facebook-share.jpg" alt="Share on Facebook" />
                     		<span>Share on Facebook</span>
                         </a>
                     </li>
-                    <li class="col-6 col-sm-3">
-                    	<a>
+                    <li class="col-6 col-sm-3 col-md-4">
+                    	<a href="mailto:?subject=Check out this competition from MyNyte!&body=Hey, check out this great new competition from MyNyte, it's at the top of their Facebook Page: www.facebook.com/MyNyte">
                         	<img src="../../sneak-preview/img/free-section/email-share.jpg" alt="Share via E-mail" />
                     		<span>Share via E-mail</span>
                         </a>
@@ -114,6 +124,9 @@
             <section class="last">
             	<h2 class="section-break">Download MyNyte - Bedford's Evening-planning App...</h2>
                 <p>If you haven't already, make sure to download MyNyte, the all-in-one evening-planning app based in Bedford, from the Apple or the Google Play app stores. Plan your whole evening in Bedford, from booking restaurant tables &amp; event tickets, to finding out cinema showing times &amp; even booking taxis.</p>
+            </section>
+            <section class="red last">
+                <a target ="_blank" href="../rules/">** PLEASE MAKE SURE TO READ OUR OFFICIAL RULES FOR THE MYNYTE.CO.UK FREE IPAD GIVEAWAY **</a>
             </section>
             <?php include_once("../templates/footer-template.php"); ?>
             <script type="text/javascript" src="../../bootstrap/js/bootstrap.min.js"></script>
